dd9faf7976ec05803774b76a474280f4683e0219
Sajjad Toomari <toomaris@gmail.com>
2018-12-21 22:54:11 +0330

    Add project files.

---
+++ ProductIdChanger/ProductIdChanger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace ProductIdService
+{
+    public class ProductIdChanger
+    {
+        public static bool ChangeProductID(string ProductID)
+        {
+            try
+            {
+                RegistryKey localKey;
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                    localKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", true);
+                    localKey.SetValue("ProductId", ProductID, RegistryValueKind.String);
+                    return true;
+                }
+                else
+                {
+                    localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                    localKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", true);
+                    localKey.SetValue("ProductId", ProductID, RegistryValueKind.String);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static string SeeProductID()
+        {
+            try
+            {
+                RegistryKey localKey;
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+                    return localKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
+                        .GetValue("ProductId").ToString();
+                }
+                else
+                {
+                    localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                    return localKey.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
+                        .GetValue("ProductId").ToString();
+                }
+            }
+            catch
+            {
+                return "Error Try Running App as Administrator";
+            }
+        }
+    }
+}
---
+++ TS-UNBANNER/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Management;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using ProductIdService;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProductIdChanger.ChangeProductID(txtProductId.Text) == true)
+            {
+                MessageBox.Show("Changed Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Error Try Running App as Administrator");
+            }
+            txtProductId.Text = ProductIdChanger.SeeProductID();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            #region UnComment This Code On 32it System's
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                MessageBox.Show("Your windows is 64bit please download 64bit verison.");
+                System.Diagnostics.Process.Start("http://lerphub.com/");
+                Application.Current.Shutdown();
+            }
+
+            #endregion
+
+            txtProductId.Text = ProductIdChanger.SeeProductID();
+           
+        }
+        private void BtnAboutUs_Click(object sender, RoutedEventArgs e)
+        {
+            AboutUsWindow a = new AboutUsWindow();
+            a.ShowDialog();
+        }
+    }
+}
